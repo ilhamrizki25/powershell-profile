199bd16e0b3b1c3a2b9e4cc2fa5d2106016fbf79
Ilham Rizki Julianto <ilhamrizkijulianto@gmail.com>
2022-02-11 10:24:20 +0700

    remove unnecessary script on user_profile.ps1

--- user_profile.ps1
+++ user_profile.ps1
@@ -1,14 +1,9 @@
-# set PowerShell to UTF-8
-# [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
-
 # Prompt
 Import-Module -Name Terminal-Icons
 Import-Module posh-git
-# Import-Module oh-my-posh
 Import-Module PSFzf
 $omp_config = Join-Path $PSScriptRoot ".\dubidam.omp.json"
 oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
-
 
 # PSReadLine
 Set-PSReadLineOption -PredictionViewStyle ListView
@@ -18,15 +13,10 @@ Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
 
-# Env
-# $env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
-
 # Alias
 Set-Alias -Name vim -Value nvim
 Set-Alias ll ls
 Set-Alias g git
-# Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
-# Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
 
 # Utilities
 function which ($command) {
